440a8120b58dd7371190755278bc9cb7a1b4b89b
Medhansh-Hinduja <medhs2011@gmail.com>
2022-08-29 16:01:13 -0400

    faster fibonacci implemented

--- Week 2/fibonacci.py
+++ Week 2/fibonacci.py
@@ -7,10 +7,21 @@ def fibo_slow(n) :
     else :
         return (fibo_slow(n-1) + fibo_slow(n-2))
 
+def fibo_better(n) :
+    A = []
+    A.append(0)
+    A.append(1)
+    for i in range(2, n+1) :
+        A.append(A[i-1] + A[i-2])
+    return A[n]
+
 def main() :
     while(True) :
         n = int(input("Enter the value of n:\n"))
+        print("Slow approach")
         print("The " + str(n) + "th fibonacci number is: " + str(fibo_slow(n)))
+        print("Better approach")
+        print("The " + str(n) + "th fibonacci number is: " + str(fibo_better(n)))
 
-if __name__ == '__main__':   
+if __name__ == '__main__':
     main()
